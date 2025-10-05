7e2628bf98a243dceaa88dbf12e751bb6776c4e5
mamdouh <mamdouhibr67@gmail.com>
2025-10-05 03:43:16 +0300

    add theme to angular nx app

---
+++ apps/esa/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Button } from "./Button/button";
+
+@Component({
+  imports: [RouterModule, Button],
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss',
+})
+export class App {
+  protected title = 'esa';
+}
